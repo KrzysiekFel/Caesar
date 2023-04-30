2e84e069f06ba26627c8450d17f1461985f0574a
Krzysiek <krzysztof.feliksiak@gmail.com>
2023-04-30 14:48:57 +0200

    feat: add decrypting method

--- manager.py
+++ manager.py
@@ -18,14 +18,14 @@ class Manager:
             encrypted_message = self.encrypt()
             self.memory["enc_messages"].append(encrypted_message)
         elif choice_number == 2:
-            decrypted_message = self.encrypt()
+            decrypted_message = self.decrypt()
             self.memory["dec_messages"].append(decrypted_message)
         elif choice_number == 3:
             self.program_is_working = False
 
     def encrypt(self) -> str:
         message_to_encrypt = input("Please provide message to encrypt: ").strip()
-        method = int(input("What encrypting method you would like to use:\n1. ROT13\n2. ROT47: ").strip())
+        method = int(input("What encrypting method would you like to use:\n1. ROT13\n2. ROT47: ").strip())
         alphabet = 'abcdefghijklmnoprstuwxyz'
         encrypted_message = []
 
@@ -42,10 +42,30 @@ class Manager:
             else:
                 new_letter_index = (alphabet.index(letter) + shift) % len(alphabet)
                 encrypted_message.append(alphabet[new_letter_index])
+        print(''.join(encrypted_message))
         return ''.join(encrypted_message)
 
-    def decrypt(self, message: str, method: str) -> str:
-        pass
+    def decrypt(self) -> str:
+        message_to_decrypt = input("Please provide message to decrypt: ").strip()
+        method = int(input("What decrypting method would you like to use:\n1. ROT13\n2. ROT47: ").strip())
+        alphabet = 'abcdefghijklmnoprstuwxyz'
+        decrypted_message = []
+
+        if method == 1:
+            shift = 13
+        elif method == 2:
+            shift = 47
+        else:
+            raise NotImplementedError
+
+        for letter in message_to_decrypt:
+            if letter == ' ':
+                decrypted_message.append(letter)
+            else:
+                new_letter_index = (alphabet.index(letter) - shift) % len(alphabet)
+                decrypted_message.append(alphabet[new_letter_index])
+        print(''.join(decrypted_message))
+        return ''.join(decrypted_message)
 
     def write_to_file(self):
         pass
