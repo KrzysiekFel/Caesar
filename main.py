95f90829283769c1e722e546bd81112e59271b68
Krzysiek <krzysztof.feliksiak@gmail.com>
2023-04-30 14:39:39 +0200

    feat: add encrypting method

--- main.py
+++ main.py
@@ -8,13 +8,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# TODO: encrypt method
-# TODO: decrypt method
-# TODO: file handler
-# TODO: datasclass
-
-# git branch feature/Add_Manager
-# print(encrypt('abc xyz', 'rot13'))
-# print(encrypt('abc xyz', 'rot47'))
--- manager.py
+++ manager.py
@@ -1,32 +1,42 @@
 class Manager:
     def __init__(self):
-        self.memory_buffer = []
+        self.memory= {"enc_messages": [], 'dec_messages': []}
         self.program_is_working = True
 
-    def start(self):
+    def start(self) -> None:
         while self.program_is_working:
-            self.show_menu()
-            self.execute()
+            choice_number = self.show_menu()
+            self.execute(choice_number)
 
-    def show_menu(self):
-        pass
+    def show_menu(self) -> int:
+        print("Caesar cipher\n1. Encrypt a message\n2. Decrypt a message\n3. Exit")
+        choice_number = int(input("Which action do you choose: ").strip())
+        return choice_number
 
-    def execute(self):
-        pass
+    def execute(self, choice_number: int) -> None:
+        if choice_number == 1:
+            encrypted_message = self.encrypt()
+            self.memory["enc_messages"].append(encrypted_message)
+        elif choice_number == 2:
+            decrypted_message = self.encrypt()
+            self.memory["dec_messages"].append(decrypted_message)
+        elif choice_number == 3:
+            self.program_is_working = False
 
-    def encrypt(self, message: str, method: str) -> str:
+    def encrypt(self) -> str:
+        message_to_encrypt = input("Please provide message to encrypt: ").strip()
+        method = int(input("What encrypting method you would like to use:\n1. ROT13\n2. ROT47: ").strip())
         alphabet = 'abcdefghijklmnoprstuwxyz'
         encrypted_message = []
-        method = method.strip().lower()
 
-        if method == 'rot13':
+        if method == 1:
             shift = 13
-        elif method == 'rot47':
+        elif method == 2:
             shift = 47
         else:
             raise NotImplementedError
 
-        for letter in message:
+        for letter in message_to_encrypt:
             if letter == ' ':
                 encrypted_message.append(letter)
             else:
@@ -35,4 +45,10 @@ class Manager:
         return ''.join(encrypted_message)
 
     def decrypt(self, message: str, method: str) -> str:
+        pass
+
+    def write_to_file(self):
+        pass
+
+    def read_from_file(self):
         pass
