fd2005913a1ac35599103184b84cb486ab83ba7e
Krzysiek <krzysztof.feliksiak@gmail.com>
2023-05-01 12:49:01 +0200

    feat: add memory buffer

--- func/encrypt_decrypt.py
+++ func/encrypt_decrypt.py
@@ -1,4 +1,5 @@
 import string
+from typing import Tuple
 
 
 class CryptoProcesses:
@@ -6,14 +7,21 @@ class CryptoProcesses:
         self.crypto_type = crypto_type
         self.alphabet = string.ascii_lowercase
 
-    def start(self):
+    def start(self) -> Tuple[str, str]:
         message, rot_type = self.get_data_for_operation()
         returned_message = self.encrypt_decrypt_process(message, rot_type, self.crypto_type)
         return returned_message, rot_type
 
-    def get_data_for_operation(self):
+    def get_data_for_operation(self) -> Tuple[str, str]:
+        get_data_running = True
         message = input(f"Please provide message for {self.crypto_type}: ").strip()
-        rot_type = input(f"What {self.crypto_type} method would you like to use ROT13 or ROT47: ").strip().lower()
+        rot_type = ''                                                                                                   # TODO: znow, jak nie zdefiniuje czegos to podswietla w returnie
+        while get_data_running:
+            rot_type = input(f"What {self.crypto_type} method would you like to use ROT13 or ROT47: ").strip().lower()
+            if rot_type in ['rot13', 'rot47']:
+                get_data_running = False
+            else:
+                print("ROT type not existing, provide ROT type again.")
         return message, rot_type
 
     def encrypt_decrypt_process(self, message: str, rot_type: str, crypto_type) -> str:
@@ -29,4 +37,6 @@ class CryptoProcesses:
             else:
                 new_letter_index = (self.alphabet.index(letter) + shift) % len(self.alphabet)
                 encrypted_message.append(self.alphabet[new_letter_index])
-        return ''.join(encrypted_message)
+        final_message = ''.join(encrypted_message)
+        print(f"{crypto_type.title()} finished. Message after {crypto_type}: {final_message}")
+        return final_message
--- func/manager.py
+++ func/manager.py
@@ -20,45 +20,52 @@ class CryptoElem:
     crypto_type: str
 
     def __repr__(self):
-        pass
+        return f"{self.message: <18}{self.crypto_type: <15}{self.rot_type: <15}"
 
 
 class Buffer:
     memory: List[CryptoElem] = []
 
+    def show_memory_buffer(self):
+        print("Printing current memory buffer.")
+        print('=' * 42)
+        print(f"{'MESSAGE': <18}{'AFTER': <15}{'ROT TYPE': <15}")
+        for crypto_elem in self.memory:
+            print(crypto_elem)
+        print('=' * 42)
+
 
 class Manager:
     def __init__(self):
-        buffer = Buffer()
-        self.memory = buffer.memory
+        self.buffer = Buffer()
         self.program_is_working = True
 
     def start(self) -> None:
+        print("*" * 31)
+        print(cipher_ascii)
         while self.program_is_working:
             choice_number = self.show_menu()
             self.execute(choice_number)
 
-    def get_input(self):
+    def get_input(self) -> int:
         choice_number = int(input("Which action do you choose: ").strip())
         if choice_number in range(1, 8):
             return choice_number
         else:
-            print(f"Provided option: {choice_number}, not such option. Choose again.\n")
+            print(f"Provided option: {choice_number}, not such option. Choose again.")
 
     def show_menu(self) -> int:
         run_menu = True
-        choice_number = 0                                                                                      # PYTANIE, czy tak to tutaj mam dołożyć? jak nie ma to podswietla return
-        print("*" * 31)
-        print(cipher_ascii)
-        print("*" * 31)
+        choice_number = 0                                                                                      # TODO: czy tak to tutaj mam dołożyć? jak nie ma to podswietla return
         while run_menu:
+            print("*" * 31)
             print("1. Encrypt a message\n2. Decrypt a message\n3. Show memory buffer\n4. Clear memory buffer\n"
                   "5. Write buffer to file\n6. Read file to memory buffer\n7. Exit")
             try:
                 choice_number = self.get_input()
                 run_menu = False
             except ValueError:
-                print("Please provide number instead of string.\n")
+                print("Please provide number instead of string.")
         return choice_number
 
     def execute(self, choice_number: int) -> None:
@@ -70,7 +77,7 @@ class Manager:
                 crypto_type = "decrypting"
                 self.crypto_execution(crypto_type)
             case 3:
-                pass
+                self.buffer.show_memory_buffer()
             case 4:
                 pass
             case 5:
@@ -81,8 +88,10 @@ class Manager:
                 print("Good bye!")
                 self.program_is_working = False
 
-    def crypto_execution(self, crypto_type):
+    def crypto_execution(self, crypto_type: str) -> None:
         crypto_process = CryptoProcesses(crypto_type)
         final_message, rot_type = crypto_process.start()
         crypto_elem = CryptoElem(final_message, rot_type, crypto_type)
-        self.memory.append(crypto_elem)
+        self.buffer.memory.append(crypto_elem)
+        print("Message added to memory buffer.")
+
