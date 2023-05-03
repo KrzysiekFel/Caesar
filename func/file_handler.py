23ffecc21df4063b6ed34b9e77ce6f136b6260c3
Krzysiek <krzysztof.feliksiak@gmail.com>
2023-05-03 17:31:49 +0200

    test(file_handler): add tests for file handler

--- func/file_handler.py
+++ func/file_handler.py
@@ -19,13 +19,15 @@ class FileHandler:
 
     def write_to_file(self, crypto_list: List[CryptoElem], file_name: str) -> None:
         file_path = self.folder_path + file_name + ".json"
+        result = {"memory_buffer": [self.crypto_object_to_dict(crypto) for crypto in crypto_list]}
         with open(file_path, "a") as outfile:
-            json.dump([self.crypto_object_to_dict(crypto) for crypto in crypto_list], outfile)
+            json.dump(result, outfile)
         print(f"Buffer has been written to: {file_path}")
 
     def read_from_file(self, file_name: str) -> List[CryptoElem]:
         file_path = self.folder_path + file_name + ".json"
         with open(file_path) as json_file:
-            list_of_dicts = json.load(json_file)
+            loaded_json_dict = json.load(json_file)
+        list_of_dicts = loaded_json_dict["memory_buffer"]
         print(f"Buffer has been updated from: {file_path}")
         return [self.dict_to_crypto_object(crypto_dict) for crypto_dict in list_of_dicts]
--- tests/test_cipher.py
+++ tests/test_cipher.py
@@ -54,3 +54,30 @@ class TestBuffer:
         assert self.buffer.memory == []
 
 
+class TestFileHandler:
+    def setup_method(self):
+        self.handler = FileHandler()
+        self.fake_file_path = "json_files/test_file.json"
+        self.file_cont = '{"memory_buffer": ' \
+                         '[{"message": "test1", "rot_type": "rot13", "crypto_type": "encrypting"}, ' \
+                         '{"message": "test2", "rot_type": "rot47", "crypto_type": "decrypting"}]} '
+
+    def test_should_return_list_of_objects_when_reading_from_json_file(self):
+        with patch("builtins.open", mock_open(read_data=self.file_cont)) as mock_file:
+            crypto_list = self.handler.read_from_file("test_file")
+            mock_file.assert_called_once_with(self.fake_file_path)
+            assert len(crypto_list) == 2
+            assert crypto_list[0].message == "test1"
+            assert crypto_list[0].rot_type == "rot13"
+            assert crypto_list[0].crypto_type == "encrypting"
+            assert crypto_list[1].message == "test2"
+            assert crypto_list[1].rot_type == "rot47"
+            assert crypto_list[1].crypto_type == "decrypting"
+
+    def test_should_correctly_write_memory_buffer_to_json_file(self):
+        crypto_list = [CryptoElem("test1", "rot13", "encrypting"), CryptoElem("test2", "rot47", "decrypting")]
+        with patch("builtins.open", mock_open()) as mock_file:
+            self.handler.write_to_file(crypto_list, "test_file")
+            mock_file.assert_called_once_with(self.fake_file_path, 'a')
+
+            #mock_file().write.assert_called_once_with(self.file_cont)
